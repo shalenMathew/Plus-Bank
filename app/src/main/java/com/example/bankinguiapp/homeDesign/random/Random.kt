6a9e9df71572f9e83f4d893a8cb19317fdc9e0f8
shalenmj <shalenmj@gmail.ocm>
2025-06-09 12:33:17 +0530

    updated

--- app/src/main/java/com/example/bankinguiapp/homeDesign/random/Random.kt
+++ app/src/main/java/com/example/bankinguiapp/homeDesign/random/Random.kt
@@ -1,2 +1,5 @@
 package com.example.bankinguiapp.homeDesign.random
 
+fun isPalindrome(input: String): Boolean {
+    return input == input.reversed()
+}
--- app/src/test/java/com/example/bankinguiapp/homeDesign/random/RandomKtTest.kt
+++ app/src/test/java/com/example/bankinguiapp/homeDesign/random/RandomKtTest.kt
@@ -1,4 +1,18 @@
+package com.example.bankinguiapp.homeDesign.random
+
 import org.junit.Assert.*
+import org.junit.Test
+
 class RandomKtTest {
-  
+
+
+    @Test
+    fun isPalindrome() {
+
+        assertTrue(isPalindrome("madam"))
+        assertFalse(isPalindrome("hello"))
+        assertTrue(isPalindrome(""))
+
+    }
+
 }
